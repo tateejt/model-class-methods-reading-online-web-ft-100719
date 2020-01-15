ec4c29d2b62cffac663fbe995d4bda39f4a77963
Emery Tate <tateejt@gmail.com>
2020-01-15 07:11:46 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,15 @@ class PostsController < ApplicationController
     @posts = Post.all
      if !params[:author].blank?
       @posts = Post.where(author: params[:author])
-    elsif !params[:date].blank?
-      if params[:date] == "Today"
-        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      elsif !params[:date].blank?
+        if params[:date] == "Today"
+          @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+        else
+          @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+        end
       else
-        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+        @posts = Post.all
       end
-    else
-      @posts = Post.all
-    end
   end
 
   def show
