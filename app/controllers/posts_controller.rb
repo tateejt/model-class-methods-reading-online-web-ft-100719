bbe0084a89b4f6fab522235f7069ddf51b0e968a
Emery Tate <tateejt@gmail.com>
2020-01-15 07:18:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
   
   def index
-    # provide a list of authors to the view for the filter control
     @authors = Author.all
    
-    # filter the @posts list based on user input
     if !params[:author].blank?
       @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
@@ -14,7 +12,6 @@ class PostsController < ApplicationController
         @posts = Post.old_news
       end
     else
-      # if no filters are applied, show all posts
       @posts = Post.all
     end
   end
