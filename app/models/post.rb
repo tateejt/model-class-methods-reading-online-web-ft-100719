2a1762e863f249c350ce22b2531426d94f6053a8
Emery Tate <tateejt@gmail.com>
2020-01-15 07:17:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,12 +10,12 @@ class Post < ActiveRecord::Base
   end
   
   def self.from_today
-  where("created_at >=?", Time.zone.today.beginning_of_day)
-end
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
  
-def self.old_news
-  where("created_at <?", Time.zone.today.beginning_of_day)
-end
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 
   private
 
